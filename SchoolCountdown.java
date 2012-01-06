f15777e083621b39bd6c1639555c0519fa1d5967
Aditya Vaidya <kroq.gar78@gmail.com>
2012-01-06 17:53:16 -0600

    Added the logic to return messages if Summer Break is the closest holiday.

--- SchoolCountdown.java
+++ SchoolCountdown.java
@@ -290,13 +290,14 @@ public class SchoolCountdown
     	int[] untilSummer = timeRemaining( new GregorianCalendar() , summer.date );
     	String schoolEndMsg = "Only " + untilSummer[0] + " day" + (untilSummer[0]==1 ? "": "s" ) + ", " + untilSummer[1] + " hour" + (untilSummer[1]==1 ? "": "s" ) + ", " + untilSummer[2] + " minute" + (untilSummer[2]==1 ? "": "s" ) + ", and " + untilSummer[3] + " second" + (untilSummer[3]==1 ? "": "s" ) + " until " + summer.name + "!";
     	String tooltip = tooltip = ( ( untilSummer[1] > 12 ? untilSummer[0]+1: untilSummer[0] ) + " day" + (untilSummer[0]==1 ? "":"s") + " until school is over!" );
-		return new Object[]{schoolEndMsg,untilSummer};
+		return new Object[]{schoolEndMsg,untilSummer,tooltip};
     }
 	public static String[] generateMessages( ArrayList<Holiday> holidays )
 	{
 		if( holidays.get(0).equals(holidays.get(holidays.size()-1) ) )
 		{
-			
+			Object[] results = generateSummerMessage(holidays.get(holidays.size()-1));
+			return ( new String[]{ (String)(results[0]) , (String)(results[2]) } );
 		}
 		return generateMessages( holidays.get(0) , holidays.get(holidays.size()-1) );
 	}
