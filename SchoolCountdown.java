337aba32472fa6bc8d0fc9de6446d6bada6fef6a
Aditya Vaidya <kroq.gar78@gmail.com>
2012-02-17 18:42:25 -0600

    Turned tooltip message into an if-then statement in hopes of fixing the '1 days' bug.

--- SchoolCountdown.java
+++ SchoolCountdown.java
@@ -237,7 +237,9 @@ public class SchoolCountdown
 		String earliestMsg = "Only " + untilClosest[0] + " day" + (untilClosest[0]==1 ? "": "s" ) + ", " + untilClosest[1] + " hour" + (untilClosest[1]==1 ? "": "s" ) + ", " + untilClosest[2] + " minute" + (untilClosest[2]==1 ? "": "s" ) + ", and " + untilClosest[3] + " second" + (untilClosest[3]==1 ? "": "s" ) + " until " + earliest.name + " and";
 		
 		// do rounding, choose which day to count to, and set tooltip at same time!
-		String tooltip = ( (untilSummer[0] <= 90 ? (String)(summerResults[2]) : (( untilClosest[1] > 12 ? untilClosest[0]+1: untilClosest[0]) + " day" + (untilClosest[0]==1 ? "":"s" ) + " until the closest holiday!" )  ) );
+		if( untilSummer[0] <= 90 )
+			String tooltip = ( untilClosest[1] > 12 ? untilClosest[0]+1: untilClosest[0]) + " day" + (untilClosest[0]==1 ? "":"s" ) + " until the closest holiday!";
+		else String tooltip = (String)summerResults[2];
 		
 		return new String[]{ earliestMsg , (String)(summerResults[0]) , tooltip }; //messages[0],messages[1]=timer text; messages[2]=tooltip
 	}
